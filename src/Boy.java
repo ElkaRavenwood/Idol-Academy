19c9ab1d8ca323bbd81ce6e9b405e1d26e513c4f
Jamie W <jamie.won.2000@gmail.com>
2018-12-01 16:09:27 -0500

    Updated boy class, reflects script changes

--- src/Boy.java
+++ src/Boy.java
@@ -1,24 +1,20 @@
 // Import statements
-import java.util.ArrayList;
 
 public class Boy {
 	
 	// Properties
 	String name;
 	int points;
-	ArrayList <String> conv;
 	
 	public Boy () {
 		this.points = 0;
-		this.conv = new ArrayList <String> ();
 		this.name = "";
 	}
 	
 	
-	public Boy (String name, ArrayList <String> conv) {
+	public Boy (String name) {
 		this.name = name;
 		this.points = 0;
-		this.conv = conv;
 	}
 	
 
